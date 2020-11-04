9d5112d7a636eefc30f520c000a23681aad96fce
IisAnh <nguye141@uwindsor.ca>
2020-11-04 17:28:34 -0500

    Created Unity Project | Started Paint Scene basis | Using TextMeshPro assets

---
+++ Assignment4/Unity Assignment 4/Assets/SceneScripts/Game.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public int CurrentHighscore;
+
+    public GameObject CurrentBrush;
+    public GameObject CurrentHighscoreTextObject;
+
+    private TMPro.TextMeshProUGUI CurrentHighscoreText;
+
+    private void Start()
+    {
+        CurrentHighscoreText = CurrentHighscoreTextObject.GetComponent<TMPro.TextMeshProUGUI>();
+        UpdateHighscore(0);
+        
+    }
+
+    public void UpdateHighscore(int score)
+    {
+        CurrentHighscore = score;
+        CurrentHighscoreText.text = score.ToString();
+    }
+
+}
---
+++ Assignment4/Unity Assignment 4/Assets/SceneScripts/Highscore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highscore : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI CurrentHighscore;
+    public void UpdateHighscore(int score)
+    {
+        CurrentHighscore.text = score.ToString();
+    }
+}
