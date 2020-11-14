e36925927915acbf33f0c0fe4fb9400f715b1989
Cassandra <owlemailu@gmail.com>
2020-11-14 18:05:26 -0500

    Getting up to date version (cloned Anh's branch).

---
+++ Assignment4/Assets/SceneScripts/Exit.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class Exit : MonoBehaviour
+{
+    public GameObject GameObject;
+    private Game GameScriptInstance;
+
+    void Start()
+    {
+        GameScriptInstance = GameObject.GetComponent<Game>();
+    }
+
+    public void ExitButton()
+    {
+        SaveToPlayerPrefs();
+        SceneManager.LoadScene(0);
+    }
+
+    private void SaveToPlayerPrefs()
+    {
+        if(!PlayerPrefs.HasKey("Paint_Highscore"))
+        {
+            Debug.Log("Has no Key in Registry...");
+            PlayerPrefs.SetInt("Paint_Highscore", GameScriptInstance.CurrentHighscore);
+        }
+        else
+        {
+            if(PlayerPrefs.GetInt("Paint_Highscore") < GameScriptInstance.CurrentHighscore)
+            {
+                Debug.Log("Has Key in Registry...");
+                Debug.Log(PlayerPrefs.GetInt("Paint_Highscore") + " < " + GameScriptInstance.CurrentHighscore);
+
+                PlayerPrefs.SetInt("Paint_Highscore", GameScriptInstance.CurrentHighscore);
+            }
+        }
+
+        Debug.Log("Starting Save...");
+        PlayerPrefs.Save();
+        Debug.Log("Saved");
+    }
+}
