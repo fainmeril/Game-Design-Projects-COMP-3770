e4ec77724031ad46956317bdd9f1610d26a955f9
Cassandra <owlemailu@gmail.com>
2020-11-15 03:20:18 -0500

    Finished Painting game.

--- Assignment4/Assets/SceneScripts/Clickable.cs
+++ Assignment4/Assets/SceneScripts/Clickable.cs
@@ -9,11 +9,17 @@ public class Clickable : MonoBehaviour
 {
     public GameObject GameObject;
     private Game GameScriptInstance;
+    public Transform prefab;
+    public GameObject spawnedShapesParent;
+    public Material defaultPaint;
+
+    private Vector3 nextPos;
 
     // Start is called before the first frame update
     void Start()
     {
         GameScriptInstance = GameObject.GetComponent<Game>();
+        prefab.GetComponent<MeshRenderer>().material = defaultPaint;
     }
 
     // Update is called once per frame
@@ -24,7 +30,8 @@ public class Clickable : MonoBehaviour
 
     private void MouseHandler()
     {
-        if (Input.GetMouseButtonDown((int)MouseButton.LeftMouse) && EventSystem.current.IsPointerOverGameObject() == false)
+        //if (Input.GetMouseButtonDown((int)MouseButton.LeftMouse) && EventSystem.current.IsPointerOverGameObject() == false)
+        if (Input.GetMouseButton((int)MouseButton.LeftMouse) && EventSystem.current.IsPointerOverGameObject() == false)
         {
             Debug.Log(MouseButton.LeftMouse);
 
@@ -35,20 +42,34 @@ public class Clickable : MonoBehaviour
                 PrintObjectName(hitTemp);
                 PaintTagReader(hitTemp);
             }
-        }
-        else if (Input.GetMouseButtonDown((int)MouseButton.RightMouse) && EventSystem.current.IsPointerOverGameObject() == false)
+        } //else if (Input.GetMouseButtonDown((int)MouseButton.RightMouse))// && EventSystem.current.IsPointerOverGameObject() == true)
+        else if (Input.GetMouseButton((int)MouseButton.RightMouse))// && EventSystem.current.IsPointerOverGameObject() == true)
         {
             Debug.Log(MouseButton.RightMouse);
-
+            
             GameObject hitTemp = RayCasting();
 
-            if(hitTemp != null)
+            if (hitTemp != null)
             {
                 PrintObjectName(hitTemp);
-                PaintTagReader(hitTemp);
+                //if (hitTemp.tag == "SpawnedObject")
+                    KillObject(hitTemp);
             }
-           
+
         }
+        /*       else if (Input.GetMouseButtonDown((int)MouseButton.RightMouse) && EventSystem.current.IsPointerOverGameObject() == false)
+               {
+                   Debug.Log(MouseButton.RightMouse);
+
+                   GameObject hitTemp = RayCasting();
+
+                   if (hitTemp != null)
+                   {
+                       PrintObjectName(hitTemp);
+                       PaintTagReader(hitTemp);
+                   }
+
+               }*/
     }
 
     private GameObject RayCasting()
@@ -61,7 +82,9 @@ public class Clickable : MonoBehaviour
         {
             if(hit.transform != null)
             {
-                return hit.transform.gameObject;
+                nextPos = hit.point;
+                
+                return hit.collider.gameObject;
             }
             else
             {
@@ -86,6 +109,7 @@ public class Clickable : MonoBehaviour
         {
             Debug.Log(gameObject.tag);
 
+
             UpdateBrush(gameObject);
         }
         else if (gameObject.tag == "SpawnedObject")
@@ -96,7 +120,7 @@ public class Clickable : MonoBehaviour
         {
             Debug.Log(gameObject.tag);
 
-            DrawSphere();
+            DrawSphere(gameObject);
         }
         else if(gameObject.tag == "UIElement")
         {
@@ -105,17 +129,47 @@ public class Clickable : MonoBehaviour
 
     }
 
-    private void UpdateBrush(GameObject gameObject)
-    {
-        GameScriptInstance.CurrentBrush = gameObject;
+    private void KillObject(GameObject gameObject) {
+        Debug.Log(gameObject.tag);
+
+        if (gameObject.tag == "SpawnedObject")
+        {
+            GameScriptInstance.UpdateHighscore(GameScriptInstance.CurrentHighscore - 1);
+
+            gameObject.GetComponent<KillThis>().KillThisObject();
+
+            //if (gameObject != null)
+             //   Destroy(gameObject.gameObject);
+        }
     }
 
-    private void DrawSphere()
+
+    private void UpdateBrush(GameObject gameObject)
     {
+        //GameScriptInstance.CurrentBrush = gameObject;
+        prefab.GetComponent<MeshRenderer>().material = gameObject.GetComponent<MeshRenderer>().material;
+
+    }
+
+    private void DrawSphere(GameObject gameObject)
+    {
+        //        nextPosX = Math.Round(Input.mousePosition.x, 0);
+        //        nextPosY = Math.Round(Input.mousePosition.y, 0);
+        //nextPosX = Math.Round(, 0);
+        //nextPosY = Math.Round(, 0);
+
         GameScriptInstance.UpdateHighscore(GameScriptInstance.CurrentHighscore + 1);
         //I think there needs to be a plane check with plane in (x, y) so that we are not generating spheres within each other at that exact point. I think
         //The tagReader method logic can handle it where it specifies the "background" tag should be a good way to see if the area is empty for sphere generation
         // I think when the object is spawned you could also reference the GameScriptInstance method "UpdateHighscore" to increment / decrement the updated score. I think .Currenthighscore + 1 or -1
+
+
+    //Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+    //Instantiate(prefab, new Vector3(nextPosX, nextPosY, 0), Quaternion.identity).transform.parent = spawnedShapesParent.transform;
+    Instantiate(prefab, new Vector3(nextPos.x, nextPos.y, 0), Quaternion.identity).transform.parent = spawnedShapesParent.transform;
+
+
+
     }
 
     private void EraseSphere()
---
+++ Assignment4/Assets/Script/KillThis.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillThis : MonoBehaviour
+{
+
+    public bool killFromStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (killFromStart)
+            KillThisObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void KillThisObject() {
+        Destroy(this.gameObject);
+    }
+}
